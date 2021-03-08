71a4fbf4421694355cec1168c5831bcd7b1a9b7b
Александр Медведев <xelam11@yandex.ru>
2021-03-08 18:39:53 +0300

    second commit

--- homework.py
+++ homework.py
@@ -14,9 +14,7 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
-# current_timestamp = int(time.time())
-# headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-# params = {'from_date': current_timestamp}
+time_sleep = 900
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -27,7 +25,6 @@ logging.basicConfig(
 
 
 def parse_homework_status(homework):
-    # homework_name = homework.get(url=URL, params=params, headers=headers)
     homework_name = homework.get('homework_name')
     if homework_name is None:
         logging.error('Не удалось получить данные дз, homework_name is None')
@@ -37,7 +34,7 @@ def parse_homework_status(homework):
         verdict = 'Работа взята в ревью.'
     elif homework.get('status') == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
+    elif homework.get('status') == 'approved':
         verdict = 'Ревьюеру всё понравилось, ' \
                   'можно приступать к следующему уроку.'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
@@ -53,6 +50,11 @@ def get_homework_statuses(current_timestamp):
     try:
         homework_statuses = requests.get(url=URL, params=params,
                                          headers=headers)
+        if 'error' in homework_statuses.json():
+            logging.error(f"Ошибка распаковки json: "
+                          f"{homework_statuses.json().get('error')}",
+                          exc_ifo=True)
+
         return homework_statuses.json()
 
     except requests.RequestException as e:
@@ -64,24 +66,23 @@ def send_message(message, bot):
 
 
 def main():
-    # проинициализировать бота здесь
     bot = Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = int(time.time())
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
+
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(
                     new_homework.get('homeworks')[0]), bot)
-            # обновить timestamp
             current_timestamp = new_homework.get('current_date',
                                                  current_timestamp)
-            time.sleep(600)  # опрашивать раз в десять минут
+            time.sleep(time_sleep)
 
-        except requests.RequestException as e:
+        except Exception as e:
             print(f'Бот столкнулся с ошибкой: {e}')
-            time.sleep(5)
+            time.sleep(time_sleep)
             logging.error(f'Произошло исключение: {e}', exc_ifo=True)
 
 
